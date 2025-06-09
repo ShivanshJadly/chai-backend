0dc98e84ab770e4a1ec2ae5aca6192cfd3b07fd5
ShivanshJadly <shivanshjadly0@gmail.com>
2025-06-09 19:36:10 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+// Approch 1 of connecting MongoDB 
+/*
+const app = express();
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(err)=>{
+            console.log("ERROR: ",err);
+            throw err;
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App listening on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("ERROR: ",error);
+        throw error;
+    }
+})()
+
+*/
