e5d7c7867ffee49c5381b0531acfbadf499d35e3
ShivanshJadly <shivanshjadly0@gmail.com>
2025-06-11 13:50:40 +0530

    refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, refreshToken, registerUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -23,5 +23,6 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/refresh-token").post(refreshToken);
 
 export default router;
